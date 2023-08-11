04e40515e2bd609d7b69e3bdcf597dbdc363f005
GabrielTomohiro <gabtom2005@gmail.com>
2023-08-11 11:20:59 -0300

    inicio node

---
+++ index.js
@@ -0,0 +1,11 @@
+var n1 = 10
+var n2 = 5
+var ad = n1 + n2
+var sub = n1 - n2
+var mult = n1 * n2
+var div = n1/n2
+
+console.log("adição = " + ad)
+console.log("subtração = " + sub)
+console.log("multiplicação = " + mult)
+console.log("divisão = " + div)
